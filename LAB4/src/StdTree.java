99a50907453552fcc42e7820868f8e87a977a619
Prasanna Rodrigo <rodrigo.rajitha@gmail.com>
2015-03-23 21:36:17 +0530

    change StdTree.java

--- LAB4/src/StdTree.java
+++ LAB4/src/StdTree.java
@@ -5,5 +5,22 @@ public class StdTree {
 
     public static void main(String [] args){
 
+        BTree<Course> courseBTree = new BTree<Course>();
+
+        Course CO321 = new Course("CO321");
+        Course CO322 = new Course("CO322");
+        Course CO323 = new Course("CO323");
+        Course CO324 = new Course("CO324");
+        Course CO325 = new Course("CO325");
+        Course EE386 = new Course("EE386");
+
+        courseBTree.insert(CO321);
+        courseBTree.insert(CO322);
+        courseBTree.insert(CO323);
+        courseBTree.insert(CO324);
+        courseBTree.insert(CO325);
+        courseBTree.insert(EE386);
+
+        courseBTree.in_order();
     }
 }
