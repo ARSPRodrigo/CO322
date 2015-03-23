e0a5c6edfdf66d869ba2b1afc5e0b015efb20246
Prasanna Rodrigo <rodrigo.rajitha@gmail.com>
2015-03-23 15:43:27 +0530

    change Course.java

--- LAB4/src/Course.java
+++ LAB4/src/Course.java
@@ -1,6 +1,25 @@
+import java.util.Random;
+
 /*
  * Created by Prasanna on 3/23/2015.
  */
 public class Course {
 
+    String courseName;
+    double gpa;
+
+    public void Course(String name){
+
+        double rangeMin = 2;
+        double rangeMax = 4;
+
+        this.courseName = name;
+        Random r = new Random();
+        this.gpa = (double) Math.round((rangeMin + (rangeMax - rangeMin) * r.nextDouble())*100)/100;
+    }
+
+    public void print(){
+        System.out.print(courseName+": ");
+        System.out.println(gpa);
+    }
 }
